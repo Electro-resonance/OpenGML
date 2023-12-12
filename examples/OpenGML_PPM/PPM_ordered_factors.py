f951a14f39100efdf15e03ad02546a037dd55330
Electro-resonance <martin.timms@electrozest.co.uk>
2023-12-12 00:01:12 +0000

    Add Phase Prime Metric (PPM) to OpenGML
    Integrate Phase Prime Metric (PPM) into OpenGML, implementing custom functions
    (developed by Martin Timms) based on Figure 3.2 in Dr Anirban Bandyopadhyay's Nanobrain book.
    
    The commit includes the addition of an ordered factor function, demonstrating its capability
    to provide a comprehensive list of ordered factors for integers.
    
    The PPM is constructed using the ordered factors and primes, followed by its projection
    onto a cylindrical projection for visualization.
    
    Additionally, the PPM order is now adjustable, enabling the selective removal of components
    for visualizing geometric variants of the Phase Prime Metric.

---
+++ examples/OpenGML_PPM/PPM_ordered_factors.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Created By  : Martin Timms
+# Created Date: 11th December 2023
+# License: BSD-3-Clause License
+# Organisation: OpenGML.org/
+# Project: https://github.com/Electro-resonance/OpenGML
+# Description: 3D Projection of Phase Prime Metric (PPM)
+#
+# Example Usage of Ordered Factor Function
+#
+# This script demonstrates an example usage of the 'ordered_factors' function
+# from the 'prime_functions' library within the OpenGML project. It iterates through
+# a range of numbers and computes their ordered factors, displaying the count and
+# the factor combinations.
+#
+# Ensure the paths to the OpenGML project and prime_functions are correctly specified
+# based on your system configuration.
+# =============================================================================
+
+import math
+import sys
+
+sys.path.append("../../src/OpenGML")  # AddOpenGML path
+sys.path.append("../../src/OpenGML/prime_functions")  # AddOpenGML path
+
+from prime_functions import ordered_factors
+
+# Example usage or ordered factor function from prime_functions library:
+
+for n in range (1,108+1):
+    ordered_factors_count,ordered_factors_combinations = ordered_factors(n)
+    print(f"OF({n}) = {ordered_factors_count}, Factors: {ordered_factors_combinations}")
